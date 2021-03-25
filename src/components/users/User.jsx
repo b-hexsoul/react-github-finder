26680264e3bf1f3597ea56527cccf5a752124274
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-03-25 15:59:39 -0700

    'User Component'

---
+++ src/components/users/User.jsx
@@ -0,0 +1,102 @@
+import React, { Component, Fragment } from "react";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
+import Repos from "../repos/Repos";
+
+class User extends Component {
+  componentDidMount() {
+    const user = this.props.match.params.user;
+    this.props.getUser(user);
+    this.props.getUserRepos(user);
+  }
+
+  render() {
+    const {
+      login,
+      name,
+      avatar_url,
+      location,
+      bio,
+      company,
+      blog,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable,
+    } = this.props.user;
+
+    const { loading, repos } = this.props;
+
+    if (loading) return <Spinner />;
+
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back to search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              alt="profile"
+              className="round-img"
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Work: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Site: </strong> <a href={blog}>{blog}</a>
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
+        <Repos repos={repos} />
+      </Fragment>
+    );
+  }
+}
+export default User;
