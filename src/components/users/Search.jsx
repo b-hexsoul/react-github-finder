1d990e914194fdebc92c0a11f3530796c6495cbb
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-03-26 00:52:15 -0700

    Revert "Updates"
    This reverts commit a1ad3b02f90b1c2b4161256772678a9b04da74b5.

---
+++ src/components/users/Search.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
+import AlertContext from "../../context/alert/alertContext";
+
+const Search = () => {
+  const [text, setText] = useState("");
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+  const { users, clearUsers, searchUsers } = githubContext;
+
+  const onChange = (e) => setText(e.target.value);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      setAlert("Please enter something", "light");
+    } else {
+      searchUsers(text);
+      setText("");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users.."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {users.length > 0 && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;
