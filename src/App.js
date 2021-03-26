007b7a2539adfc3cefdf92897b1ee9b87a5cb17b
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-03-26 00:15:57 -0700

    'Update for Context and Routes'

--- src/App.js
+++ src/App.js
@@ -1,65 +1,36 @@
-import { Component } from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar.component";
-import Users from "./components/users/Users";
-import Search from "./components/users/Search";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 import Alert from "./components/layout/Alert";
+import About from "./pages/About";
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
 
 import "./App.css";
-import axios from "axios";
+import User from "./components/users/User";
 
-class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-    alert: null,
-  };
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await axios.get(
-  //     `https://api.github.com/users?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}`
-  //   );
-
-  //   this.setState({ loading: false, users: res.data });
-  // }
-
-  searchUsers = async (text) => {
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}`
-    );
-
-    this.setState({ loading: false, users: res.data.items, alert: null });
-  };
-
-  clearUsers = () => {
-    this.setState({ users: [], loading: false });
-  };
-
-  setAlert = (message, type) => {
-    this.setState({ alert: { msg: message, type } });
-
-    setTimeout(() => this.setState({ alert: null }), 3000);
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <Navbar title="Github Finder" icon="fab fa-github" />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            showClear={this.state.users.length > 0 ? true : false}
-            clearUsers={this.clearUsers}
-            setAlert={this.setAlert}
-          />
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar title="Github Finder" icon="fab fa-github" />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/user/:user" component={User} />
+                <Route path="*" component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
